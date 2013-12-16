ad2cc842548fc6ef2670fc205d9e8ea5574a253e
Dale Campbell <oshuma@gmail.com>
2013-12-16 02:33:28 -0500

    Removing build version in about.xml

--- weechat-android/src/main/java/com/ubergeek42/WeechatAndroid/WeechatAboutActivity.java
+++ weechat-android/src/main/java/com/ubergeek42/WeechatAndroid/WeechatAboutActivity.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2012 Keith Johnson
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -32,9 +32,6 @@ public class WeechatAboutActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
-        TextView tv = (TextView) this.findViewById(R.id.versionID);
-        tv.setText(com.ubergeek42.WeechatAndroid.BuildConfig.VERSION_BANNER);
-        
     }
 
 }
